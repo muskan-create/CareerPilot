2259663862c07acbce6c63d063aa6cdce87c8137
Muskan Prajapati <muskanprajapati0001@gmail.com>
2026-09-21 15:04:58 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -25,8 +25,8 @@ app.get('/', (req, res) => {
   res.send('CareerPilot Backend is running!')
 })
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
